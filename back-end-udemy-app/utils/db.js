e729e45c47034a7ff82a161137bf56724b62f897
DESKTOP-C0L9UHR\vunam <vunam1218@gmail.com>
2021-07-25 01:08:00 +0700

    setup login fb and google

--- back-end-udemy-app/utils/db.js
+++ back-end-udemy-app/utils/db.js
@@ -1,11 +1,11 @@
 const knex = require("knex")({
   client: "mysql2",
   connection: {
-    host: "us-cdbr-east-04.cleardb.com",
-    user: "b24e3594d725bb",
-    password: "fdad89a8",
-    database: "heroku_112bc1e92b24ce0",
-    port: 3306,
+    host: "127.0.0.1",
+    user: "root",
+    password: "root",
+    database: "education",
+    port: 8889,
   },
   pool: {
     min: 0,
